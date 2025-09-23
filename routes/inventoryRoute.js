7cc407462b9d93286deb7d8a6babfd93b7d7fe47
Henri Gonzalez <henrijo123@gmail.com>
2025-09-23 18:57:26 -0300

    New content

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -6,4 +6,9 @@ const invController = require("../controllers/invController");
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
 
+/* ****************************************
+ * Route to build vehicle detail view
+ **************************************** */
+router.get("/detail/:id", utilities.handleErrors(invController.buildDetail));
+
 module.exports = router;
